8ba464d73df8f8a96d688396580b82ac027b30c4
Omor Faruk shrabon <omarfaruksrabon@gmail.com>
2026-02-07 21:51:15 +0600

    Time: 1 ms (87.79%), Space: 55.2 MB (51.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let need = target - nums[i];
+
+        if (map.has(need)) {
+            return [map.get(need), i];
+        }
+
+        map.set(nums[i], i);
+    }
+}
