b052d88e372df7579362f2450e70a8276b9be621
Omor Faruk shrabon <omarfaruksrabon@gmail.com>
2026-02-07 23:22:15 +0600

    Time: 4 ms (85.37%), Space: 69.9 MB (68%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,14 @@
+function productExceptSelf(nums){
+  let res = new Array(nums.length).fill(1);
+  let left = 1, right = 1;
+
+  for(let i=0;i<nums.length;i++){
+    res[i] *= left;
+    left *= nums[i];
+  }
+  for(let i=nums.length-1;i>=0;i--){
+    res[i] *= right;
+    right *= nums[i];
+  }
+  return res;
+}
